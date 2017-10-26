fa48d3a99fa6e2ed647a78d6736ce4cfb978c002
Lewis Vine <lew.vine@gmail.com>
2017-10-26 13:18:24 +0000

    API works for registered apps, doesn't for non-registered apps

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   resources :registered_applications
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
 
   root 'welcome#index'
 
